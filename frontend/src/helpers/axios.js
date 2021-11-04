bda13f73db4c3b0b99310ddd6e424c72b74a674b
Md. Iubullah Biplobe <biplobe6@gmail.com>
2021-11-04 10:57:43 +0600

    Fixed: CORS related issue.

--- frontend/src/helpers/axios.js
+++ frontend/src/helpers/axios.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 console.log(process.env.NODE_ENV);
-const baseURL = "https://rjeapptest.herokuapp.com/api/";
+const baseURL = `${window.location.protocol}//${window.location.host}/api/`
 
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
-  timeout: 5000,
   headers: {
     Authorization: localStorage.getItem("access_token")
       ? "JWT " + localStorage.getItem("access_token")
@@ -17,7 +16,6 @@ const axiosInstance = axios.create({
 
 export const fileAxios = axios.create({
   baseURL: baseURL,
-  timeout: 5000,
   headers: {
     Authorization: localStorage.getItem("access_token")
       ? "JWT " + localStorage.getItem("access_token")
@@ -29,7 +27,6 @@ export const fileAxios = axios.create({
 
 const refreshAxios = axios.create({
   baseURL: baseURL,
-  timeout: 5000,
   headers: {
     "Content-Type": "application/json",
     accept: "application/json",
