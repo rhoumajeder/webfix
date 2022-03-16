f3889b84b979b5d8f385d618cd5735b022b74975
rhoumajeder <chawkijeder@gmail.com>
2022-03-17 00:24:43 +0100

    captcha false

--- rjeproject/settings.py
+++ rjeproject/settings.py
@@ -18,7 +18,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-CAPTCHA = True
+CAPTCHA = False
 
 
 # Quick-start development settings - unsuitable for production
